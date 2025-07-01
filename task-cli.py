53c661ddb8094544b7922ee7fa40d5c20f617efd
José Dantas <josedantasjdneto@gmail.com>
2025-07-01 16:59:42 -0300

    added commands and modified created_at/updated_at formatting

--- task-cli.py
+++ task-cli.py
@@ -8,13 +8,65 @@ class Task:
         self.id = len(tasks) + 1
         self.description = description
         self.status = "todo"
-        self.created_at = str(datetime.now())
+        self.created_at = str(datetime.now().strftime(":%Y-%m-%d %H:%M:%S"))
         self.updated_at = self.created_at
 
     def update_description(self, description):
         self.description = description
-        self.updated_at = str(datetime.now())
+        self.updated_at = str(datetime.now().strftime(":%Y-%m-%d %H:%M:%S"))
 
     def update_status(self, status):
         self.status = status
-        self.updated_at = str(datetime.now())
+        self.updated_at = str(datetime.now().strftime(":%Y-%m-%d %H:%M:%S"))
+
+
+def find_task_by_id(id):
+    for task in tasks:
+        if task.id == id:
+            return task
+    else:
+        return None
+
+
+def add_task(description):
+    task = Task(description)
+    tasks.append(task)
+
+
+def update(id, description):
+    task = find_task_by_id(id)
+    if not task:
+        return
+    task.update_description(description)
+
+
+def mark_in_progress(id):
+    task = find_task_by_id(id)
+    if not task:
+        return
+    task.update_status("in-progress")
+
+
+def mark_done(id):
+    task = find_task_by_id(id)
+    if not task:
+        return
+    task.update_status("done")
+
+
+def list(status=None):
+    for task in tasks:
+        if status and task.status != status:
+            continue
+        print(
+            f"{'-' * 33}\n"
+            f"ID         : {task.id}\n"
+            f"Description: {task.description}\n"
+            f"Status     : {task.status}\n"
+            f"Created at : {task.created_at}\n"
+            f"Updated at : {task.updated_at}\n"
+            f"{'-' * 33}"
+        )
+
+
+
